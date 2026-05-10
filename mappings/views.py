c600da75ffd2714490db7af9f904de5ee4f93506
Mukil <smukil007@gmail.com>
2026-05-10 14:17:12 +0530

    Implement patient-specific doctor mapping retrieval API

--- mappings/views.py
+++ mappings/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from .models import PatientDoctorMapping
 from .serializers import MappingSerializer
@@ -12,3 +14,14 @@ class MappingViewSet(viewsets.ModelViewSet):
     serializer_class = MappingSerializer
 
     permission_classes = [IsAuthenticated]
+
+    @action(detail=False, methods=['get'], url_path='patient/(?P<patient_id>[^/.]+)')
+    def get_doctors_by_patient(self, request, patient_id=None):
+
+        mappings = PatientDoctorMapping.objects.filter(
+            patient_id=patient_id
+        )
+
+        serializer = self.get_serializer(mappings, many=True)
+
+        return Response(serializer.data)
